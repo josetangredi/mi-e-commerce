2075ab25eb5fac2ff1e154599919e6d0a7d506a9
josetangredi <tangredijosefina@gmail.com>
2024-12-16 15:03:11 -0300

    Agregando polyfills para módulos de Node.js

--- craco.config.js
+++ craco.config.js
@@ -1,24 +1,16 @@
-const webpack = require("webpack");
+// craco.config.js
+const path = require('path');
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.fallback = {
-        ...webpackConfig.resolve.fallback,
-        os: require.resolve("os-browserify/browser"),
-        path: require.resolve("path-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        vm: require.resolve("vm-browserify"), // Polyfill para 'vm'
-      };
-
-      return webpackConfig;
+    resolve: {
+      fallback: {
+        "buffer": require.resolve("buffer/"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "vm": require.resolve("vm-browserify"),
+        "path": require.resolve("path-browserify"),
+      },
     },
-    plugins: [
-      new webpack.ProvidePlugin({
-        process: "process/browser", // Soporte para 'process'
-        Buffer: ["buffer", "Buffer"], // Soporte para 'Buffer'
-      }),
-    ],
   },
 };
